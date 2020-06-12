d6b79c9ae6ff00c1241277a64050fb862214c7f0
molecanvas@gmail.com <morel_chang@trend.com.tw>
2020-06-12 17:14:59 +0800

    Add git-prompt.sh to init-env.sh

--- init-env.sh
+++ init-env.sh
@@ -7,7 +7,8 @@ curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/mas
 curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bash_profile
 
-curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >> ~/.bash_profile
+curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+echo "source ~/.git-prompt.sh" >>  ~/.bash_profile
 
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.vimrc -o ~/.vimrc
 mkdir -p ~/.vim/colors/ && curl -s -o - https://raw.githubusercontent.com/romainl/Apprentice/master/colors/apprentice.vim > ~/.vim/colors/apprentice.vim
