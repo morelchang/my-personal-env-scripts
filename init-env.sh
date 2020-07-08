61b176612de25b678ad5e83ae52d1eff64761caf
molecanvas@gmail.com <morel_chang@trend.com.tw>
2020-07-08 09:36:17 +0800

    replace git-prompt

--- init-env.sh
+++ init-env.sh
@@ -7,12 +7,16 @@ curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/mas
 curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bash_profile
 
-curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-echo "source ~/.git-prompt.sh" >> ~/.bash_profile
-echo "PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '" >> ~/.bash_profile
+#curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+#echo "source ~/.git-prompt.sh" >> ~/.bash_profile
+#echo "PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '" >> ~/.bash_profile
 
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.vimrc -o ~/.vimrc
 mkdir -p ~/.vim/colors/ && curl -s -o - https://raw.githubusercontent.com/romainl/Apprentice/master/colors/apprentice.vim > ~/.vim/colors/apprentice.vim
 echo "export CLICOLOR=1" >> ~/.bash_profile
+
+# gitprompt.sh
+echo "GIT_PROMPT_ONLY_IN_REPO=1" >>  ~/.bash_profile
+echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bash_profile
 
 source ~/.bash_profile
