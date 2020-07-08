07fd1ca92eb5bc50bcaf2a5de2ee7f92e618ee5e
molecanvas@gmail.com <morel_chang@trend.com.tw>
2020-07-08 09:52:27 +0800

    source script in bashrc for screen

--- init-env.sh
+++ init-env.sh
@@ -5,7 +5,7 @@ curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/mas
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.gitconfig -o ~/.gitconfig
 
 curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-echo "source ~/.git-completion.bash" >> ~/.bash_profile
+echo "source ~/.git-completion.bash" >> ~/.bashrc
 
 #curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 #echo "source ~/.git-prompt.sh" >> ~/.bash_profile
@@ -13,10 +13,10 @@ echo "source ~/.git-completion.bash" >> ~/.bash_profile
 
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.vimrc -o ~/.vimrc
 mkdir -p ~/.vim/colors/ && curl -s -o - https://raw.githubusercontent.com/romainl/Apprentice/master/colors/apprentice.vim > ~/.vim/colors/apprentice.vim
-echo "export CLICOLOR=1" >> ~/.bash_profile
+echo "export CLICOLOR=1" >> ~/.bashrc
 
 # gitprompt.sh
-echo "GIT_PROMPT_ONLY_IN_REPO=1" >>  ~/.bash_profile
-echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bash_profile
+echo "GIT_PROMPT_ONLY_IN_REPO=1" >>  ~/.bashrc
+echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bashrc
 
 source ~/.bash_profile
