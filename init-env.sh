32689d9f8103226d4d633efd2c909b4055946d11
molecanvas@gmail.com <morel_chang@trend.com.tw>
2020-07-10 15:36:30 +0800

    Fix git-prompt

--- init-env.sh
+++ init-env.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 ntpdate time.stdtime.gov.tw
 
+# screen and git config
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.screenrc -o ~/.screenrc
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.gitconfig -o ~/.gitconfig
 
+# git completion
 curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bashrc
 
-#curl -s -o - https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-#echo "source ~/.git-prompt.sh" >> ~/.bash_profile
-#echo "PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '" >> ~/.bash_profile
+# git prompt
+curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+echo "source ~/.git-prompt.sh" >> ~/.bashrc
+echo "PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '" >> ~/.bashrc
 
+# vim config
 curl -s https://raw.githubusercontent.com/morelchang/my-personal-env-scripts/master/.vimrc -o ~/.vimrc
 mkdir -p ~/.vim/colors/ && curl -s -o - https://raw.githubusercontent.com/romainl/Apprentice/master/colors/apprentice.vim > ~/.vim/colors/apprentice.vim
 echo "export CLICOLOR=1" >> ~/.bashrc
 
-# gitprompt.sh
-echo "GIT_PROMPT_ONLY_IN_REPO=1" >>  ~/.bashrc
-echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bashrc
-
-source ~/.bash_profile
+source ~/.bashrc
